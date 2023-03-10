8962c80e939d40f6c99faa7f9d03f389bc6e365f
Alejo Verhagen <verhagenalejo@gmail.com>
2023-03-10 19:41:02 -0300

    Agregar archivos de proyecto.

---
+++ UniApiBackend/DataAccess/UniversityContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniApiBackend.Models.DataModels;
+
+namespace UniApiBackend.DataAccess
+{
+    public class UniversityContext: DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options): base(options) 
+        { 
+
+        }
+        // Add DbSets (Table of our Data Base) "<User>" es la conexion a la clase User.cs, Users = nombre de la tabla
+        public DbSet<User>? Users { get; set; } 
+    }
+}
---
+++ UniApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniApiBackend.Models.DataModels
+{
+    public class Curso
+    {
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(280)]
+        public string DescriptionShort { get; set; } = string.Empty;
+        public string DescriptionLong { get; set; } = string.Empty;
+        public string ObjetivoPincipal { get; set; } = string.Empty;
+        public string ObjetivosGenerales { get; set; } = string.Empty;
+        public string Requisitos { get; set; } = string.Empty;
+        public enum Nivel
+        {
+            Basico, Intermedio, Avanado
+        }
+
+    }
+}
